e755d206eae0bcfea59e0f54815d2186526aa301
kk <kk@DESKTOP-5VILT95>
2023-12-11 18:14:33 -0800

    Add project files.

---
+++ DummyProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, sum;
+
+            Console.WriteLine($"Enter numbers");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            sum = num1 + num2;
+            Console.WriteLine($"Sum of Numbers {sum}");
+            Console.ReadLine();
+
+        }
+    }
+}

a696ab5ea7e6bbfda93f95debf4c2a8f9f2a880b
muhammadmudassar12 <muhammadmudassar7353@outlook.com>
2023-12-11 18:59:03 -0800

    fix: add sum function

--- DummyProject/Program.cs
+++ DummyProject/Program.cs
@@ -15,10 +15,15 @@ namespace DummyProject
             Console.WriteLine($"Enter numbers");
             num1 = Convert.ToInt32(Console.ReadLine());
             num2 = Convert.ToInt32(Console.ReadLine());
-            sum = num1 + num2;
+            sum = sumNo(num1, num2);
             Console.WriteLine($"Sum of Numbers {sum}");
             Console.ReadLine();
 
+        }
+        public static int sumNo(int num1,int num2)
+        {
+            
+            return num1 + num2;
         }
     }
 }
